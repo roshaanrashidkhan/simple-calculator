0817c3b72693e32c4e6cf310a1284cb109fb9213
Roshaan Rashid Khan <roshaan.rashid.r50@gmail.com>
2024-03-22 17:24:38 +0500

    first-project using TypeScript and Inquirer

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+// The above path is known as shabang which is used to make it available to anyone within the same network
+import inquirer from "inquirer";
+const calculator = await inquirer.prompt([
+    { message: "Enter a First Number:", type: "number", name: "firstNumber" },
+    { message: "Enter a Second Number:", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the following operators to perform operations:",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponentiation", "Modulus"],
+    },
+]);
+if (calculator.operator === "Addition") {
+    console.log("Your value is:", calculator.firstNumber + calculator.secondNumber);
+}
+else if (calculator.operator === "Subtraction") {
+    console.log("Your value is:", calculator.firstNumber - calculator.secondNumber);
+}
+else if (calculator.operator === "Multiplication") {
+    console.log("Your value is:", calculator.firstNumber * calculator.secondNumber);
+}
+else if (calculator.operator === "Division") {
+    console.log("Your value is:", calculator.firstNumber / calculator.secondNumber);
+}
+else if (calculator.operator === "Exponentiation") {
+    console.log("Your value is:", calculator.firstNumber ** calculator.secondNumber);
+}
+else if (calculator.operator === "Modulus") {
+    console.log("Your value is:", calculator.firstNumber % calculator.secondNumber);
+}
+else {
+    console.log("Please select a valid operator");
+}
